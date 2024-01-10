0a2aeed3aab8f59fe7ea4fc1e07a4c60c43785b5
Arfaan <90722310+mdarfaanbaig@users.noreply.github.com>
2024-01-10 13:02:40 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,14 +2,8 @@
 SQLyog Community Edition- MySQL GUI v7.01 
 MySQL - 5.0.27-community-nt : Database - algorithmvisualizer
 *********************************************************************
-*/
+*/
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`algorithmvisualizer` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
@@ -33,5 +27,4 @@ CREATE TABLE `register` (
 
 insert  into `register`(`id`,`username`,`email`,`password`,`mobileno`,`address`) values (1,'a','yashsalvi1999@gmail.com','a','9930090883','vedika soc sagar nagar vikhroli park site'),(2,'a','a@gmail.com','a','5845458545','a');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
